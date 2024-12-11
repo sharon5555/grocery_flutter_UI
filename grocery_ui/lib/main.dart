4a9bcfbfa5958475778ecece6235ca22a43a287d
Naza <sharon.doris54@gmail.com>
2024-12-11 03:29:27 +0100

    created the screens folder, added  the first screen file and  also added images to the assets folder

---
+++ grocery_ui/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_ui/screens/first_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstScreen(),
+    );
+  }
+}
