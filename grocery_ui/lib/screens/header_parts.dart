f481cff92a645f112a45c1af70adc9fca6ce655e
Naza <sharon.doris54@gmail.com>
2024-12-12 17:21:48 +0100

    updated header parts file

--- grocery_ui/lib/screens/header_parts.dart
+++ grocery_ui/lib/screens/header_parts.dart
@@ -12,56 +12,107 @@ class _HeaderPartsState extends State<HeaderParts> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
+        topHeader(),
+        const SizedBox(
+          height: 30,
+        ),
+        title(),
+          const SizedBox(
+          height: 21,
+        ),
+        Container(
+          height: 55,
+          margin: const EdgeInsets.symmetric(horizontal: 15),
+          padding: const EdgeInsets.only(right: 5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: const Row(
             children: [
-              // For Menu
-            Material(
-              color: Colors.black12,
+              Expanded(child: TextField()),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Padding title() {
+    return const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          Text(
+            'Hi Nador', 
+            style: TextStyle(
+              fontWeight: FontWeight.bold, 
+              color: primaryColors, 
+              fontSize: 18,
+            ),
+          ),
+          Text(
+            'Find your food', 
+            style: TextStyle(
+              fontWeight: FontWeight.bold, 
+              color: Colors.black, 
+              fontSize: 34,
+            ),
+          ),
+        ],),
+      );
+  }
+
+  Padding topHeader() {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Row(
+          children: [
+            // For Menu
+          Material(
+            color: Colors.black12,
+            borderRadius: BorderRadius.circular(10),
+            child: InkWell(
+              onTap: () {},
               borderRadius: BorderRadius.circular(10),
-              child: InkWell(
-                onTap: () {},
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                  height: 40,
-                  width: 40,
-                  alignment: Alignment.center,
-                  child: const Icon(
-                    Icons.menu_open_rounded, 
-                    color: Colors.black,
-                  ),
+              child: Container(
+                height: 40,
+                width: 40,
+                alignment: Alignment.center,
+                child: const Icon(
+                  Icons.menu_open_rounded, 
+                  color: Colors.black,
                 ),
               ),
             ),
-            const Spacer(),
-            // For Location
-            const Icon(
-              Icons.location_on, 
-              color: primaryColors,
-              size: 18,
-            ),
-            const Text(
-              'Ajah Lekki', 
-              style: TextStyle(
-                fontWeight: FontWeight.bold, 
-                color: Colors.black38, 
-              ),
-            ),
-            const Spacer(),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.asset(
-                "assets/face.webp", 
-                height: 40, 
-                width: 40,
-              ),
-            ),
-          ],
           ),
-        )
-      ],
-    );
+          const Spacer(),
+          // For Location
+          const Icon(
+            Icons.location_on, 
+            color: primaryColors,
+            size: 18,
+          ),
+          const Text(
+            'Ajah Lekki', 
+            style: TextStyle(
+              fontWeight: FontWeight.bold, 
+              color: Colors.black38, 
+            ),
+          ),
+          const Spacer(),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(
+              "assets/face.jpg", 
+              height: 40, 
+              width: 40,
+            ),
+          ),
+        ],
+        ),
+      );
   }
 }
