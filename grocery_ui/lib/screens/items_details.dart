203f2e2dab2dc1cb781370c2949105f41e348c4e
Naza <sharon.doris54@gmail.com>
2024-12-15 21:48:18 +0100

    updated the item detail file and the items model file

--- grocery_ui/lib/screens/items_details.dart
+++ grocery_ui/lib/screens/items_details.dart
@@ -4,60 +4,61 @@ import 'package:grocery_ui/utils/colors.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key, required this.food});
+
   final FoodDetail food;
 
   @override
   State<DetailPage> createState() => _DetailPageState();
 }
-int quantity = -1;
 
 class _DetailPageState extends State<DetailPage> {
+  int quantity = 1; // Initialize quantity to a positive value
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primaryColors,
       body: ListView(
         children: [
-          const SizedBox(
-            height: 15,
-          ),
+          const SizedBox(height: 15),
           // Detail Header.....
           detailItemHeader(),
           // For Image
           detailImage(),
           Container(
             color: Colors.white,
-            padding: const EdgeInsets.symmetric(
-              horizontal: 15,
-            ),
+            padding: const EdgeInsets.symmetric(horizontal: 15),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
                   children: [
-                    Expanded(child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // For Name
-                        Text(
-                          widget.food.name,
-                    maxLines: 1,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      fontSize: 34,
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // For Name
+                          Text(
+                            widget.food.name,
+                            maxLines: 1,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 34,
+                            ),
+                          ),
+                          // For price
+                          Text(
+                            '\$${widget.food.price}',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                              color: primaryColors,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    ),
-                    // For price
-                    Text('\$${widget.food.price}',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold, 
-                      fontSize: 24, 
-                      color: primaryColors,
-                    ),
-                    ),
-                    ],
-                    ),),
                     // For Item Quantity
                     Material(
                       color: primaryColors,
@@ -65,42 +66,125 @@ class _DetailPageState extends State<DetailPage> {
                       child: Row(
                         children: [
                           IconButton(
-                            onPressed: (){
-                              if(quantity >1){
-                                quantity -=1;
+                            onPressed: () {
+                              if (quantity > 1) {
                                 setState(() {
-                                  
+                                  quantity -= 1;
                                 });
                               }
-                            }, 
-                            icon: const Icon(
-                              Icons.remove, 
-                              color: Colors.white),
-                            ),
-                            const SizedBox(width: 4),
-                            Text(
-                              "$quantity", 
+                            },
+                            icon: const Icon(Icons.remove, color: Colors.white),
+                          ),
+                          const SizedBox(width: 4),
+                          Text(
+                            "$quantity",
                             style: const TextStyle(
-                              color: Colors.white, 
-                              fontSize: 18,
+                              color: Colors.white,
+                              fontSize: 25,
                             ),
                           ),
-                          const SizedBox(
-                            width: 4,
-                          ),
+                          const SizedBox(width: 4),
                           IconButton(
-                            onPressed: (){
-                              
-                            }, 
-                            icon: const Icon(
-                              Icons.add, 
-                              color: Colors.white),
-                            ),
+                            onPressed: () {
+                              setState(() {
+                                quantity += 1;
+                              });
+                            },
+                            icon: const Icon(Icons.add, color: Colors.white),
+                          ),
                         ],
                       ),
                     ),
                   ],
-                )
+                ),
+                const SizedBox(height: 27),
+                Row(
+                  children: [
+                    // For Rating......
+                    const Icon(
+                            Icons.star, 
+                            color: Colors.amber, 
+                          ),
+                          const SizedBox(
+                            width: 4,
+                          ),
+                          Text(
+                            widget.food.rate.toString(), 
+                            style: const TextStyle(
+                              color: Colors.black38,
+                              fontSize: 18,
+                            ),
+                          ),
+                          const Spacer(),
+                          // For kcal..........
+                          const Icon(
+                            Icons.fiber_manual_record, 
+                            color: Colors.red,
+                          ),
+                          const SizedBox(
+                            width: 4,
+                          ),
+                          Text('${widget.food.kcal}kcal',
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                        ),
+                        const Spacer(),
+                        // For Time........
+                        const Icon(Icons.access_time_filled, 
+                        color: Colors.amber,
+                        ),
+                        const SizedBox(
+                          width: 4,
+                        ),
+                        Text(
+                            widget.food.cookingTime,
+                            maxLines: 1,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                  ],
+                ),
+                // For Description........
+                const SizedBox(
+                  height: 10,
+                ),
+                    Text(
+                            widget.food.description,
+                            style: const TextStyle(
+                              color: Colors.black54,
+                              fontSize: 16,
+                            ),
+                          ),
+                          // For Add to Cart Button........
+                          const SizedBox(
+                          height: 25,
+                          ),
+                          Material(
+                            color: primaryColors,
+                            borderRadius: BorderRadius.circular(15),
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Container(
+                                width: double.infinity,
+                                padding: const EdgeInsets.symmetric(vertical: 21),
+                                child: const Text('Add to Cart', 
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 25,
+                            ),
+                        
               ],
             ),
           ),
@@ -111,93 +195,93 @@ class _DetailPageState extends State<DetailPage> {
 
   SizedBox detailImage() {
     return SizedBox(
-          height: 300,
-          width: double.infinity,
-          child: Stack(
-            children: [
-              Positioned(
-                left: 0,
-                bottom: 0,
-                right: 0,
-                child: Container(
-                height: 150,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(35),
-                      topRight: Radius.circular(35),
-                    ),
+      height: 300,
+      width: double.infinity,
+      child: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            bottom: 0,
+            right: 0,
+            child: Container(
+              height: 150,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(35),
+                  topRight: Radius.circular(35),
                 ),
               ),
-              ),
-              Center(
-                child: Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.purple[400]!,
-                        blurRadius: 15,
-                        offset: const Offset(0, 8),
-                      ),
-                    ],
-                    borderRadius: BorderRadius.circular(250),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(250),
-                      child: Image.asset(
-                        widget.food.image, 
-                        height: 250, 
-                        width: 250, 
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ),
-            ],
+            ),
           ),
-        );
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.purple[400]!,
+                    blurRadius: 15,
+                    offset: const Offset(0, 8),
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(250),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(250),
+                child: Image.asset(
+                  widget.food.image,
+                  height: 250,
+                  width: 250,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Padding detailItemHeader() {
     return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
-            children: [
-              // For back button.....
-              Material(
-                color: Colors.white.withOpacity(0.21),
-                borderRadius: BorderRadius.circular(10),
-                child: const BackButton(color: Colors.white,),
-              ),
-              const Spacer(),
-              // For detail food
-              const Text(
-                "Detail Food", 
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Spacer(),
-              Material(
-                color: Colors.white.withOpacity(0.21),
-                borderRadius: BorderRadius.circular(10),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    height: 40,
-                    width: 40,
-                    alignment: Alignment.center,
-                    child: const Icon(
-                      Icons.favorite_border, 
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ],
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Row(
+        children: [
+          // For back button.....
+          Material(
+            color: Colors.white.withOpacity(0.21),
+            borderRadius: BorderRadius.circular(10),
+            child: const BackButton(color: Colors.white),
           ),
-        );
+          const Spacer(),
+          // For detail food
+          const Text(
+            "Detail Food",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Spacer(),
+          Material(
+            color: Colors.white.withOpacity(0.21),
+            borderRadius: BorderRadius.circular(10),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                height: 40,
+                width: 40,
+                alignment: Alignment.center,
+                child: const Icon(
+                  Icons.favorite_border,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
